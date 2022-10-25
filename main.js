dd8d6b7e8ce028ef27a77a60fee23c203c5b79cb
Roberto G O <roberto.garcia.olmos@gmail.com>
2022-10-24 23:46:57 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const mongoose= require('mongoose');
+mongoose.connect("mongodb+srv://mongoRoberto:mongoRoberto@cluster0.8vfswqi.mongodb.net/proyecto5_ATAPI");
+
+const Persona = mongoose.model('Persona',{
+    nombre:String,
+    edad:Number,
+    activo:Boolean,
+})
+
+const Mario = new Persona({
+nombre:"Juan",
+edad:17,
+activo:false,
+})
+
+Mario.save().then(() => {console.log("saves");});
+
+Persona.find({nombre:"Mario"}).then((Persona)=>{console.log(Persona)})
